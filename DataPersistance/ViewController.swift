6318aba6aee561957a2d82c0cb77648f03750037
Phani <phanindranath.wg113@gmail.com>
2019-02-12 23:03:25 -0600

    Initial commit

--- DataPersistance/ViewController.swift
+++ DataPersistance/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PhoneNumTextfield: UITextField!
+    @IBOutlet var NameTextfield: UITextField!
+     @IBOutlet weak var SaveUpdateOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let nameValue = UserDefaults.standard.value(forKey: "Name"){
+            NameTextfield.text = UserDefaults.standard.value(forKey: "Name") as? String
+            SaveUpdateOutlet.setTitle("Update", for: .normal)
+            //            print(NameTextfield.text!)
+        }
+        else{
+            UserDefaults.standard.set(NameTextfield.text, forKey: "Name")
+            //            print(NameTextfield.text!)
+        }
+        if let PhoneNum = UserDefaults.standard.value(forKey: "PhoneNumber"){
+            PhoneNumTextfield.text = UserDefaults.standard.value(forKey: "PhoneNumber") as? String
+            //            print(PhoneNumTextfield.text!)
+        }
+        else{
+            UserDefaults.standard.set(PhoneNumTextfield.text, forKey: "PhoneNumber")
+            print(PhoneNumTextfield.text!)
+        }
     }
-
-
+    
+    @IBAction func SaveUpdateButton(_ sender: Any) {
+        UserDefaults.standard.set(NameTextfield.text, forKey: "Name")
+        UserDefaults.standard.set(PhoneNumTextfield.text, forKey: "PhoneNumber")
+        viewDidLoad()
+    }
+    
 }
 
